1c02d17df985f9d61501b0f40bff49ed5efb7ce6
Chris Hammersley <chris-hammersley@users.noreply.github.com>
2023-11-12 11:45:58 -0800

    project files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wayfinding",
+  base: "/wayfinding-nov1/",
   plugins: [react()],
 })
