8254a76558311bd0ba52cf59b43a51e8a45e73a8
Chris Hammersley <chris-hammersley@users.noreply.github.com>
2023-11-12 19:48:04 -0800

    Update vite.config.js
    updated base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wayfinding-nov1/",
+  base: "/wayfinding-old2/",
   plugins: [react()],
 })
